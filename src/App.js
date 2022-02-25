674fb5a57f308817b5a367cc6c7822fdc449d6a6
Siegfred Samson <siegfredsamson@gmail.com>
2022-02-25 20:04:48 +0100

    changeMessage function for render

--- src/App.js
+++ src/App.js
@@ -4,26 +4,29 @@ import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Joy from './components/MyTest'
 import Hello from './components/Hello'
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
+
+      <Message />
       {/* <Greet/> */} {/* if Greet is exported as a default component, we can import it with any name */}
       {/* <Welcome />
       <Joy/> */}
       {/* <Hello/> */}
-      <Greet name= "Bodam" heroName= "batman">
+      {/* <Greet name= "Bodam" heroName= "batman">
         <p>this is childrens props</p>
       </Greet>
       <Greet name= "Tunde" heroName= "Superman">
         <button>Samantha</button>
       </Greet>
-      <Greet name= "Anietie" heroName= "Wonder Woman"/>
+      <Greet name= "Anietie" heroName= "Wonder Woman"/> */}
 
       {/* Using props with a class component */}
-      <Welcome name= "Bodam" heroName= "batman" />
+      {/* <Welcome name= "Bodam" heroName= "batman" />
       <Welcome name= "Tunde" heroName= "Superman" />
-      <Welcome name= "Anietie" heroName= "Wonder Woman" />
+      <Welcome name= "Anietie" heroName= "Wonder Woman" /> */}
     </div>
   );
 }
