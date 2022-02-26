ae61d2a36ab7bdfe4805c28042251017c515e20c
Siegfred Samson <siegfredsamson@gmail.com>
2022-02-26 10:21:54 +0100

    increment function created with call back function

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,9 +10,23 @@ class Counter extends Component {
       }
     }
 
+    // increment(){
+    //     this.setState({
+    //         count: this.state.count + 1
+    //     })
+    //     console.log(this.state.count)
+    // }
+
+    // Because React is Asynchrounous, we will always have the console.log 
+    // running after th setState method, hence, the logged message will be
+    // 1 less than the UI value.
+    // to fix this, we use a call back function for the console.log msg
+
     increment(){
         this.setState({
             count: this.state.count + 1
+        }, ()=>{
+            console.log('Call Back Value', this.state.count)
         })
         console.log(this.state.count)
     }
