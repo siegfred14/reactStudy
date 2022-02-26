570633c7fc9fe59bb77f262eb6e5bd13a16e7bc1
Siegfred Samson <siegfredsamson@gmail.com>
2022-02-26 09:56:51 +0100

    increment function created

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         count: 0
+      }
+    }
+
+    increment(){
+        this.setState({
+            count: this.state.count + 1
+        })
+        console.log(this.state.count)
+    }
+
   render() {
     return (
-      <div>Count</div>
+      <div>
+          <div>Count - {this.state.count}</div>
+          <button onClick={()=> this.increment()}>Increment</button>
+      </div>
     )
   }
 }
