04e4a739b878b80037d5b2bf48fdd5f2cbb2d60d
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-01 10:28:46 +0100

    EventBind.js setup created

---
+++ src/components/EventBind.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class EventBind extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         message: 'Hello'
+      }
+    }
+
+  render() {
+    return (
+      <div>
+          <div>{this.state.message}</div>
+        <button>Click</button>
+      </div>
+    )
+  }
+}
+
+export default EventBind
