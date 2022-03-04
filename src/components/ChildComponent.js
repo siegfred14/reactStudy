688af5266e9eedcdb8c27c416cf70eb68da332e9
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-04 21:32:28 +0100

    props.greetHandler added

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function ChildComponent() {
+function ChildComponent(props) {
   return (
     <div>
-        <button>Greet Parent</button>
+        <button onClick={props.greetHandler}>Greet Parent</button>
     </div>
   )
 }
