09d256487214353509edeb9ea4527bb5b62f5f22
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-05 15:02:39 +0100

    UserGreeting.js skeleton added

---
+++ src/components/UserGreeting.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class UserGreeting extends Component {
+  render() {
+    return (
+      <div>Welcome Siegfred</div>
+    )
+  }
+}
+
+export default UserGreeting
