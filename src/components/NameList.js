0d8f52a91c448bc3a959344ab178ae028da4a00f
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-06 13:52:56 +0100

    list component using mapping functions with variables

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,19 +1,24 @@
 import React from "react";
 
 function NameList() {
-  let names = ["Bruce", "Clark", "Diana"];
-  return (
-    <div>
-      {/* <h2>{names[0]}</h2>
-      <h2>{names[1]}</h2>
-      <h2>{names[2]}</h2> */}
+  //   let names = ["Bruce", "Clark", "Diana"];
+  //   return (
+  //     <div>
+  //       {/* <h2>{names[0]}</h2>
+  //       <h2>{names[1]}</h2>
+  //       <h2>{names[2]}</h2> */}
 
-      {/* Using the mapping function */}
-      {names.map((name) => (
-        <h2>{name}</h2>
-      ))}
-    </div>
-  );
+  //       {/* Using the mapping function */}
+  //       {/* {names.map((name) => (
+  //         <h2>{name}</h2>
+  //       ))} */}
+  //     </div>
+  //   );
+
+  //   Using the variables
+  const names = ["Bruce", "Clark", "Diana"];
+  const nameList = names.map((name) => <h2>{name}</h2>);
+  return <div>{nameList}</div>;
 }
 
 export default NameList;
