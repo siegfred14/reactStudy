9b644537b5d16976dfb79ca0a542cc41a5435f22
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-06 12:10:49 +0100

    If Else statement in React Conditional rendering 2

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -11,25 +11,37 @@ class UserGreeting extends Component {
       }
 
   render() {
-      if (this.state.isLoggedIn){
-          return(
-            <div>
-                <div>Welcome Siegfred</div>
-            </div>
-          )
-      }else{
-          return(
-            <div>
-                <div>Welcome Guest</div>
-            </div>
-          )
-      }
-    return (
-      <div>
-          <div>Welcome Siegfred</div>
-          <div>Welcome Guest</div>
-      </div>
-    )
+    //   Type 2 of conditional rendering
+
+    let message;
+    if (this.state.isLoggedIn){
+        message = <div>Welcome Siegfred</div>
+    }else{
+        message = <div>Welcome Guest</div>
+    }
+    return <div>{message}</div>
+
+    //   Type 1 of conditional rendering
+    //   if (this.state.isLoggedIn){
+    //       return(
+    //         <div>
+    //             <div>Welcome Siegfred</div>
+    //         </div>
+    //       )
+    //   }else{
+    //       return(
+    //         <div>
+    //             <div>Welcome Guest</div>
+    //         </div>
+    //       )
+    //   }
+
+    // return (
+    //   <div>
+    //       <div>Welcome Siegfred</div>
+    //       <div>Welcome Guest</div>
+    //   </div>
+    // )
   }
 }
 
