76ff9f02e9f5d83e3309bec3f6e62df47d5606e4
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-10 06:28:07 +0100

    Stylesheet.js structure created

--- src/components/Stylesheet.js
+++ src/components/Stylesheet.js
@@ -3,3 +3,15 @@
 // 2. Inline styling
 // 3. CSS Modules
 // 4. CSS in JS Libraries
+
+import React from "react";
+
+function Stylesheet() {
+  return (
+    <div>
+      <h1>Stylesheet</h1>
+    </div>
+  );
+}
+
+export default Stylesheet;
