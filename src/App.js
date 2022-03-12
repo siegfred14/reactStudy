86b61d50f9cd3aa50f850fa9e2b39c79f63156b0
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-12 17:19:55 +0100

    inline styling created

--- src/App.js
+++ src/App.js
@@ -11,11 +11,13 @@ import ParentComponent from "./components/ParentComponent";
 import UserGreeting from "./components/UserGreeting";
 import NameList from "./components/NameList";
 import Stylesheet from "./components/Stylesheet";
+import Inline from "./inline";
 
 function App() {
   return (
     <div className="App">
-      <Stylesheet primary={true} />
+      <Inline />
+      {/* <Stylesheet primary={true} /> */}
       {/* <NameList /> */}
       {/* <UserGreeting /> */}
       {/* <ParentComponent /> */}
--- src/inline.js
+++ src/inline.js
@@ -4,10 +4,15 @@ import React from "react";
 // instead they are specified with an object whose key is the camel case version
 // of the styling, and the value is usually a string
 
+const heading = {
+  fontSize: "72px",
+  color: "blue",
+};
+
 function Inline() {
   return (
     <div>
-      <h1>Inline</h1>
+      <h1 style={heading}>Inline</h1>
     </div>
   );
 }
