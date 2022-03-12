ff052116662ecab7434d9dbcb1f5d558ddf7a20d
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-12 17:16:24 +0100

    notes on inline styling added to inline.js

--- src/inline.js
+++ src/inline.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+// In react, inline styles are not specified as a string
+// instead they are specified with an object whose key is the camel case version
+// of the styling, and the value is usually a string
+
 function Inline() {
   return (
     <div>
