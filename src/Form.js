8486be1df28b3b9545a1d0767c337b83567e8db5
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-13 13:29:28 +0100

    value added for Form.js input element

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 
+// To convert the form from a regular html to a react component we must
+// 1. Create a contolled state that will control the value of the element
+
 class Form extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+    };
+  }
+
   render() {
     return (
       <form>
         <label>Username</label>
-        <input type="text" />
+        <input type="text" value={this.state.username} />
       </form>
     );
   }
