2337a166f3f6286d763ce64f77da2b1a0f8b43bf
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-13 13:33:39 +0100

    Handling the unchanged event

--- src/Form.js
+++ src/Form.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 // To convert the form from a regular html to a react component we must
 // 1. Create a contolled state that will control the value of the element
+// 2. Handling the unchanged event
 
 class Form extends Component {
   constructor(props) {
@@ -12,11 +13,21 @@ class Form extends Component {
     };
   }
 
+  handleUsernamechange = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+  };
+
   render() {
     return (
       <form>
         <label>Username</label>
-        <input type="text" value={this.state.username} />
+        <input
+          type="text"
+          value={this.state.username}
+          onChange={this.handleUsernamechange}
+        />
       </form>
     );
   }
