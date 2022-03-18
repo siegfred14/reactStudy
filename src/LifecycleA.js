76843a5186d50c84b0460abde37e11c39c97f683
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-18 17:59:36 +0100

    LifecycleA.js Mounting: constructor added

--- src/LifecycleA.js
+++ src/LifecycleA.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
 
 class LifecycleA extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "Siegfred",
+    };
+    console.log("LifecycleA constructor");
+  }
+
   render() {
+    console.log("LifecycleA render");
     return <div>LifecycleA</div>;
   }
 }
