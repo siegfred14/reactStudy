e5adce2e72bab241434814ff3fca3b34613a1ab7
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-18 18:10:31 +0100

    LifecycleA.js Mounting: notes added

--- src/LifecycleA.js
+++ src/LifecycleA.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 
+// methods in mounting
 class LifecycleA extends Component {
+  // method 1: constructor
   constructor(props) {
     super(props);
 
@@ -10,14 +12,17 @@ class LifecycleA extends Component {
     console.log("LifecycleA constructor");
   }
 
+  // method 2: getDerivedStateFromProps
   static getDerivedStateFromProps(props, state) {
     console.log("LifecycleA getDerivedStateFromProps");
   }
 
+  // method 3: componentDidMount
   componentDidMount() {
     console.log("LifecycleA ComponentDidMount");
   }
 
+  // method 4: render method
   render() {
     console.log("LifecycleA render");
     return <div>LifecycleA</div>;
