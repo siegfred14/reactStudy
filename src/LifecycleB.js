c28eff9487b67ba12632ce763f1338d5715596dd
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-19 12:26:58 +0100

    Mounting with LifecycleB

--- src/LifecycleB.js
+++ src/LifecycleB.js
@@ -1,1 +1,32 @@
+import React, { Component } from "react";
 
+// methods in mounting
+class LifecycleB extends Component {
+  // method 1: constructor
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "Siegfred",
+    };
+    console.log("LifecycleB constructor");
+  }
+
+  // method 2: getDerivedStateFromProps
+  static getDerivedStateFromProps(props, state) {
+    console.log("LifecycleB getDerivedStateFromProps");
+  }
+
+  // method 3: componentDidMount
+  componentDidMount() {
+    console.log("LifecycleB ComponentDidMount");
+  }
+
+  // method 4: render method
+  render() {
+    console.log("LifecycleB render");
+    return <div>LifecycleB</div>;
+  }
+}
+
+export default LifecycleB;
