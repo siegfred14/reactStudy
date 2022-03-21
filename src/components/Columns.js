d9834668dbcb58b5c9a7813c5cbe6af57786699c
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-21 09:25:19 +0100

    React.Fragments using empty tags

--- src/components/Columns.js
+++ src/components/Columns.js
@@ -3,16 +3,20 @@ import React from "react";
 function Columns() {
   const items = [];
   return (
-    <React.Fragment>
-      {items.map((item) => (
-        <React.Fragment key={item.id}>
-          <h1>Title</h1>
-          <p>{item.title}</p>
-        </React.Fragment>
-      ))}
+    // <React.Fragment>
+    //   {items.map((item) => (
+    //     <React.Fragment key={item.id}>
+    //       <h1>Title</h1>
+    //       <p>{item.title}</p>
+    //     </React.Fragment>
+    //   ))}
+    //   <td>Name</td>
+    //   <td>Siegfred</td>
+    // </React.Fragment>
+    <>
       <td>Name</td>
       <td>Siegfred</td>
-    </React.Fragment>
+    </>
   );
 }
 
