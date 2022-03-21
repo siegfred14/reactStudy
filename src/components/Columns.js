cc1e01c1864b9e3ecbcfd807a75012bf97619c99
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-21 09:24:18 +0100

    React.Fragments using key attributes

--- src/components/Columns.js
+++ src/components/Columns.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 function Columns() {
+  const items = [];
   return (
     <React.Fragment>
+      {items.map((item) => (
+        <React.Fragment key={item.id}>
+          <h1>Title</h1>
+          <p>{item.title}</p>
+        </React.Fragment>
+      ))}
       <td>Name</td>
       <td>Siegfred</td>
     </React.Fragment>
@@ -10,3 +17,8 @@ function Columns() {
 }
 
 export default Columns;
+
+// NOTES
+// Fragments can accept the key attribute when rendering a list of items
+// Key attribute is the only attribute that can be parsed to a React Fragment for now
+// React Fragments can also be rendered using an empty opening tag and an empty closing tag
