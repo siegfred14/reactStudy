69bd4f1fdab9fbace6ccb307b3d056bb472ba0b6
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-22 08:19:36 +0100

    more notes on differences btw normal and pure components

--- src/components/ParentComp.js
+++ src/components/ParentComp.js
@@ -36,3 +36,11 @@ export default ParentComp;
 // Differences btw Regular and Pure Components
 // A Regular Component does not implement the shouldComponentUpdate method.It always returns 'true' by default
 // A pure component on the other hand implements shouldComponentUpdate with a shalow props and state comparison.
+
+// PURE COMPONENT
+// A Pure component implements shouldComponentUpdate with a shallow prop and state comparison
+// It does a shallow comparison of prevState with currentState
+// and prevProps with currentProps
+//  and only when the shallow comparison indicates there is a difference will the component rerender
+
+// Because there is no change in the previous prop value and the current, the pure component does not re-render
