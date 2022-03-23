f830138f58301ad2981136a3bacadd7b958cfb52
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-23 08:40:30 +0100

    Notes on React.memo

--- src/components/MemoComp.js
+++ src/components/MemoComp.js
@@ -12,3 +12,5 @@ function MemoComp({ name }) {
 export default React.memo(MemoComp);
 
 // What pure components is to class components, memo is to functional components
+// React.memo(MemoComp); is refered to as a higher order component
+// React.memo accepts a component, adds some properties to the component and returns a new enhanced component
