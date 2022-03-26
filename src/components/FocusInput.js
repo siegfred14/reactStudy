6743bc4232a45b01f72890269fd211e61fcdc453
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-26 09:49:24 +0100

    FocusInput.js ref attribute added

--- src/components/FocusInput.js
+++ src/components/FocusInput.js
@@ -2,10 +2,15 @@ import React, { Component } from "react";
 import Input from "./Input";
 
 class FocusInput extends Component {
+  constructor(props) {
+    super(props);
+    this.componentRef = React.createRef();
+  }
+
   render() {
     return (
       <div>
-        <Input />
+        <Input ref={this.componentRef} />
         <button>Focus Input</button>
       </div>
     );
