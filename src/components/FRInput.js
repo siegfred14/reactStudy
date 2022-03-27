9bf80e6afdf177725e8552a1aab9fe6d88624c1e
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-27 18:37:06 +0100

    FRInput.js structure added

--- src/components/FRInput.js
+++ src/components/FRInput.js
@@ -1,2 +1,14 @@
 // REF FORWARDING
 // - A technique of automatically passing a ref through a constructor to one of it's children
+
+import React from "react";
+
+function FRInput() {
+  return (
+    <div>
+      <input />
+    </div>
+  );
+}
+
+export default FRInput;
