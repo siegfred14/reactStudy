3c261fb23849e52be4492967c94969e26aadffca
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-28 09:08:15 +0100

    Portal demo rendered in React Portals

--- src/components/PortalDemo.js
+++ src/components/PortalDemo.js
@@ -1,12 +1,20 @@
 // React Portals provide a way to render children into a DOM Node that exists outside the DOM heirarchy, of the parent component
 
 import React from "react";
+import ReactDOM from "react-dom";
 
 function PortalDemo() {
-  return (
+  //   return (
+  //     <div>
+  //       <h1>Portals Demo</h1>
+  //     </div>
+  //   );
+
+  return ReactDOM.createPortal(
     <div>
       <h1>Portals Demo</h1>
-    </div>
+    </div>,
+    document.getElementById("portal-root")
   );
 }
 
