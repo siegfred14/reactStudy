b402b2da89d751feabcdcc74259d2219f3b61207
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-31 11:46:55 +0100

    Counter Component created

--- src/components/ClickCounter.js
+++ src/components/ClickCounter.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
 
 export class ClickCounter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  incrementCount = () => {
+    this.setState((formerState) => {
+      return { count: formerState.count + 1 };
+    });
+  };
   render() {
+    const { count } = this.state;
     return (
       <div>
-        <button>Clicked X Times</button>
+        <button onClick={this.incrementCount}>Clicked {count} Times</button>
       </div>
     );
   }
