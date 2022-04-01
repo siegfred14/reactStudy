e8ed33efc1e2e5395866f8a7add29d12613514c6
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-01 16:53:50 +0100

    Higher order components function created

---
+++ src/components/withCounter.js
@@ -0,0 +1,14 @@
+// Higher Order Components HOC is a pattern where a function takes a component as an argument and returns another component
+// const NewComponent = higherOrderComponent(originalComponent)
+// eg const IronMan = withSuit(TonyStark)
+
+import React from "react";
+
+const UpdatedComponent = (OriginalComponent) => {
+  class NewComponent extends React.Component {
+    render() {
+      return <OriginalComponent />;
+    }
+  }
+  return NewComponent;
+};
