f4ce5d4423991f76eebd9a2cdd9663f4ded72c7b
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-06 08:35:23 +0100

    Steps to implementing a context

--- src/components/ComponentC.js
+++ src/components/ComponentC.js
@@ -1,4 +1,8 @@
 // Context provides a way to pass down data through a component tree without having to pass props down manually at every level.
+// Steps to implementing a context
+// 1. Create the context
+// 2. Provide a context value
+// 3. Consume the context value
 
 import React, { Component } from "react";
 import ComponentE from "./ComponentE";
