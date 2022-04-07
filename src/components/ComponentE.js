56b9e9255db61e65e480bc6fabb670260fc72700
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-07 20:28:54 +0100

    Alternative method, using public class field Syntax

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -3,6 +3,7 @@ import ComponentF from "./ComponentF";
 import UserContext from "./userContext";
 
 export class ComponentE extends Component {
+  static contextType = UserContext;
   render() {
     return (
       <div>
@@ -13,6 +14,11 @@ export class ComponentE extends Component {
   }
 }
 
-ComponentE.contextType = UserContext;
+// ComponentE.contextType = UserContext;
 
 export default ComponentE;
+
+// If your application supports the public class field Syntax, you can replace
+// ComponentE.contextType = UserContext;
+// with
+// static contextType = UserContext;  (Before the render method)
