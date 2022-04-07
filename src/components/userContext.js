2cf06fa267c2d71420ed7ad6f6343976f2d47420
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-07 20:19:53 +0100

    Asigning the UserContext

--- src/components/userContext.js
+++ src/components/userContext.js
@@ -21,3 +21,4 @@ export default UserContext;
 
 // Using the context type property on a class
 // 1. export default UserContext
+// 2. Assign this UserContext to the context type property on a class (Using Component E)
